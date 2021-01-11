4a4f54adb816f447a8a9bc16868049b074807595
TheEvilSocks <TheEvilSocks@gmail.com>
2021-01-11 13:02:58 +0100

    I wasted time on a 404 page
    Signed-off-by: TheEvilSocks <TheEvilSocks@gmail.com>

---
+++ 404.js
@@ -0,0 +1,19 @@
+function blub() {
+	const fishElem = document.createElement('img');
+	fishElem.classList.add(...['fish', 'dolphin']);
+	fishElem.src = 'icons/rare/dolphin.svg';
+	document.body.appendChild(fishElem);
+
+	fishElem.style.left = `${Math.random() * window.innerWidth}px`;
+	fishElem.style.bottom = `${window.innerHeight + fishElem.clientHeight + Math.random() * fishElem.clientHeight * 4}px`;
+	fishElem.style.transform = `rotate(${Math.random() * 360}deg)`;
+
+	setTimeout(() => {
+		document.body.removeChild(fishElem);
+	}, 2500);
+}
+
+setInterval(() => {
+	setTimeout(blub, Math.random() * 750);
+}, 750);
+blub();
